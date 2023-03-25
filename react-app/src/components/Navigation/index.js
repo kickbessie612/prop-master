29cb8a685aeeef0f2360039cc9ecb2fd690f4828
Yuan Wang <yuanwang612@gmail.com>
2023-03-25 09:50:57 -0700

    update icon

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,21 +4,31 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+  return (
+    <div>
+      <NavLink exact to='/'>
+        <i className='fa-sharp fa-solid fa-p'></i>&nbsp;&nbsp;&nbsp;Prop Master
+      </NavLink>
+      <NavLink exact to='/movies'>
+        Movies
+      </NavLink>
+      <NavLink exact to='/prophouses'>
+        Prophouses
+      </NavLink>
+      <NavLink exact to='/props'>
+        Props
+      </NavLink>
+
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
+    </div>
+  );
 }
 
 export default Navigation;
